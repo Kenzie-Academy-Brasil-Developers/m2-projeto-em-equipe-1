6b1e87cede3e72abefe2afe96713c83af2e38156
Renan <49.renan@gmail.com>
2023-01-17 18:24:01 -0300

    feat: Criar home e estilizar

---
+++ src/scripts/home.js
@@ -0,0 +1,35 @@
+const btn1 = document.querySelector("#btn1")
+
+btn1.addEventListener("click", () => {
+    alert("Está funcionando o click")
+})
+
+const btn2 = document.querySelector("#btn2")
+
+btn2.addEventListener("click", () => {
+    alert("Está funcionando o click")
+})
+
+const btn3 = document.querySelector("#btn3")
+
+btn3.addEventListener("click", () => {
+    alert("Está funcionando o click")
+})
+
+const btn4 = document.querySelector("#btn4")
+
+btn4.addEventListener("click", () => {
+    alert("Está funcionando o click")
+})
+
+const btn5 = document.querySelector("#btn5")
+
+btn5.addEventListener("click", () => {
+    alert("Está funcionando o click")
+})
+
+const btn6 = document.querySelector("#btn6")
+
+btn6.addEventListener("click", () => {
+    alert("Está funcionando o click")
+})
